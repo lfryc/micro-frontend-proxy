bf3d1c4e4cb78f414c398eb03ae27df66edcaeaf
Lukas Fryc <lukas.fryc@exponea.com>
2018-05-27 22:20:47 +0200

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('shared-root h1')).getText();
+  }
+}
