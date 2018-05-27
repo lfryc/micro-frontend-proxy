9d835f334a3bbe1991efd683f916abd825295fc9
Lukas Fryc <lukas.fryc@exponea.com>
2018-05-27 22:43:13 +0200

    finished angular-frontend

--- angular-frontend/app/app.module.ts
+++ angular-frontend/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SharedModule} from '../../src/app/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  template: `
+    <h3><a href="http://localhost:4200">Angular</a></h3>
+  `,
+  styles: [`
+      :host {
+        grid-area: sidebar;
+        background: #1c1733;
+        color: #8f93d9;
+      }
+  
+      a, a:active, a:visited {
+        display: block;
+        color: #8f93d9;
+        text-decoration: none;
+      }
+  
+      a:hover {
+        color: white;
+      }
+    `],
+  encapsulation: ViewEncapsulation.Native
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
