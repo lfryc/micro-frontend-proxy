2bd817538a760840526f775dc1e1e4dd95417e37
Lukas Fryc <lukas.fryc@exponea.com>
2018-05-27 22:59:20 +0200

    copied vue-frontend to react-frontend

---
+++ react-frontend/main.ts
@@ -0,0 +1,32 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(CustomElementsModule)
+  .catch(err => console.log(err));
+
+import Vue from 'vue/dist/vue.esm';
+import {CustomElementsModule} from '../src/app/custom-elements.module';
+
+Vue.config.ignoredElements = [
+  'shared-topbar',
+  'shared-sidebar'
+];
+
+const app = new Vue({
+  el: '#app',
+  data: {},
+  template: `
+    <div class="container">
+      <shared-topbar></shared-topbar>
+      <shared-sidebar></shared-sidebar>
+      <main>Vue.js micro-frontend</main>
+    </div>
+  `
+});
